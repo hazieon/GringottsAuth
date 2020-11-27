cb5748fdb6a8d3a318610f4dd83c48ee8cce8c25
Hazieon <handrew3721@gmail.com>
2020-11-27 12:10:16 +0000

    bank component working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@ import LoginButton from "../LoginButton";
 import "./App.css";
 import LogoutButton from "../LogOutButton";
 import Profile from "../Profile";
+import Bank from "../Bank";
+import banner from "./bank1.png";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Profile />
+        <img src={banner} />
         <LoginButton />
         <LogoutButton />
       </header>
---
+++ src/components/Bank/index.js
@@ -0,0 +1,46 @@
+import React, { useReducer, useState } from "react";
+import Profile from "../Profile";
+
+let initialBalance = 0;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "deposit":
+      return Number(state + action.payload);
+
+    case "withdraw":
+      return state - action.payload;
+
+    default:
+      return state;
+  }
+}
+
+function Bank() {
+  const [balance, dispatch] = useReducer(reducer, initialBalance);
+  const [input, setInput] = useState(0);
+
+  return (
+    <>
+      <h1>Welcome to Gringotts Bank</h1>
+      <Profile />
+
+      <h2>{balance}</h2>
+      <h3>{input}</h3>
+
+      <input
+        type="number"
+        onChange={(e) => setInput(Number(e.target.value))}
+        placeholder="gold transaction"
+      />
+      <button onClick={() => dispatch({ type: "deposit", payload: input })}>
+        deposit
+      </button>
+      <button onClick={() => dispatch({ type: "withdraw", payload: input })}>
+        withdraw
+      </button>
+    </>
+  );
+}
+
+export default Bank;
