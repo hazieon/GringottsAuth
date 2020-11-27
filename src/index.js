3e17dc2bd2bb9a383d0a519fef8a8448b4e7a596
Ionut <ipredoi27@gmail.com>
2020-11-27 10:43:38 +0000

    authentication with auth0 completed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-drd4u1rt.eu.auth0.com"
+    clientId="D5vzGrX5unCWgLbunvoDUqDWcukq87XN"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
